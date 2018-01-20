5687f03557a246fdfb382546b9dc19381010b9eb
yihonglei <yihonglei@baofeng.com>
2018-01-20 16:33:43 +0800

    java-thread

---
+++ java-base/java-thread/src/main/java/com/lanhuigu/safe/StatelessFactorizer.java
@@ -0,0 +1,32 @@
+package com.lanhuigu.safe;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class StatelessFactorizer implements Servlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
