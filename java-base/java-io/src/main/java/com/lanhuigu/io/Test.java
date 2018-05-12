d92d0b21546aa2f5ca631ce9844dc77a054a82aa
yihonglei <yihonglei@baofeng.com>
2018-05-12 17:59:38 +0800

    io字节和字符方式读取文件

---
+++ java-base/java-io/src/main/java/com/lanhuigu/io/Test.java
@@ -0,0 +1,20 @@
+package com.lanhuigu.io;
+
+import java.io.*;
+
+/**
+ * @author yihonglei
+ * @version 1.0.0
+ * @ClassName: Test
+ * @Package: com.lanhuigu.io
+ * @date 2018/5/8 10:50
+ */
+public class Test {
+    public static void main(String[] args) {
+        /*InputStreamReader;
+        InputStream;
+        FilterInputStream;
+        BufferedInputStream;
+        LineNumberInputStream;*/
+    }
+}
