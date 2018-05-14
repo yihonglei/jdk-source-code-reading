aef025e9046fd8c0ae8b52fb086b90d44f6ea507
yihonglei <yihonglei@baofeng.com>
2018-05-14 21:36:14 +0800

    nio

---
+++ java-base/java-nio/src/main/java/com/lanhuigu/nio/Test.java
@@ -0,0 +1,22 @@
+package com.lanhuigu.nio;
+
+import java.nio.Buffer;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+import java.nio.channels.Selector;
+
+/**
+ * @author yihonglei
+ * @version 1.0.0
+ * @ClassName: Test
+ * @Package: com.lanhuigu.nio
+ * @date 2018/5/14 10:42
+ */
+public class Test {
+    public static void main(String[] args) {
+        /*Channel;
+        Buffer;
+        Selector;
+        FileChannel;*/
+    }
+}
---
+++ java-base/java-nio/src/main/java/com/lanhuigu/nio/channel/ChannelCopyFileTest.java
@@ -0,0 +1,48 @@
+package com.lanhuigu.nio.channel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * 基于NIO完成文件复制
+ * @author yihonglei
+ * @ClassName: ChannelCopyFileTest
+ * @Package: com.lanhuigu.nio.channel
+ * @date 2018/5/14 11:35
+ * @version 1.0.0
+ */
+public class ChannelCopyFileTest {
+    public static void main(String[] args) {
+        // 源文件
+        File fromFile = new File("C:\\mycode\\hello.txt");
+        // 目标文件
+        File toFile = new File("C:\\mycode\\hello-copy.txt");
+        try (
+            // 根据源文件创建文件输入流
+            FileInputStream fis = new FileInputStream(fromFile);
+            // 根据目标文件创建文件输入流，如果文件不存在，自动创建
+            FileOutputStream fos = new FileOutputStream(toFile);
+            // 1. 获取通道
+            FileChannel inChannel = fis.getChannel();
+            FileChannel outChannel = fos.getChannel();
+         ) {
+            // 2. 分配指定大小缓冲区
+            ByteBuffer buffer = ByteBuffer.allocate(48);
+            // 3. 将通道的数据读取到缓冲区
+            while (inChannel.read(buffer) != -1) {
+                // 切换模式
+                buffer.flip();
+                // 4. 从缓冲区写入到通道
+                outChannel.write(buffer);
+                // 5. 清空缓冲区
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
