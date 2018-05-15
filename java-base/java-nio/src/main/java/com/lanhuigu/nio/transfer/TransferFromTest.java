ef49d198bbcaf219072222a0db1dc203906b46e1
yihonglei <yihonglei@baofeng.com>
2018-05-15 11:22:28 +0800

    通道间数据传输

---
+++ java-base/java-nio/src/main/java/com/lanhuigu/nio/transfer/TransferFromTest.java
@@ -0,0 +1,42 @@
+package com.lanhuigu.nio.transfer;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+/**
+ * FileChannel中transferFrom源码:
+ * public abstract long transferFrom(ReadableByteChannel src, long position, long count) throws IOException;
+ * FileChannel的transferFrom()方法可以将数据从源通道传输到FileChannel中。
+ * @author yihonglei
+ * @ClassName: TransferFromTest
+ * @Package: com.lanhuigu.nio.transfer
+ * @date 2018/5/15 11:02
+ * @version 1.0.0
+ */
+public class TransferFromTest {
+   /**
+    * 文件复制: 将fromFile.txt文件内容复制到toFile.txt中
+    */
+    public static void main(String[] args) {
+        try (
+                // 指定文件创建文件输入流
+                FileInputStream fis = new FileInputStream("C:\\mycode\\fromFile.txt");
+                // 指定文件创建文件输出流
+                FileOutputStream fos = new FileOutputStream("C:\\mycode\\toFile.txt");
+                // 获取通道
+                FileChannel fromChannel = fis.getChannel();
+                FileChannel toChannel = fos.getChannel();
+        ) {
+            // 定义position的位置为初始位置0
+            long position = 0;
+            // 获取文件总的字节数
+            long count = fromChannel.size();
+            // 从fromChannel通道缓冲区position位置开始读取count字节数写入到目标通道toChannel中
+            toChannel.transferFrom(fromChannel, position, count);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java-base/java-nio/src/main/java/com/lanhuigu/nio/transfer/TransferToTest.java
@@ -0,0 +1,41 @@
+package com.lanhuigu.nio.transfer;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+/**
+ * FileChannel的transferTo源码:
+ * public abstract long transferTo(long position, long count, WritableByteChannel target) throws IOException;
+ * transferTo()方法将数据从FileChannel传输到其他的Channel中。
+ * @author yihonglei
+ * @ClassName: TransferToTest
+ * @Package: com.lanhuigu.nio.transfer
+ * @date 2018/5/15 11:03
+ * @version 1.0.0
+ */
+public class TransferToTest {
+    /**
+     * 文件复制: 将fromFile.txt文件内容复制到toFile.txt中
+     */
+    public static void main(String[] args) {
+        try (
+                // 指定文件创建文件输入流
+                FileInputStream fromFile = new FileInputStream("C:\\mycode\\fromFile.txt");
+                // 指定文件创建文件输出流
+                FileOutputStream toFile = new FileOutputStream("C:\\mycode\\toFile.txt");
+                // 获取通道
+                FileChannel fromChannel = fromFile.getChannel();
+                FileChannel toChannel = toFile.getChannel()) {
+            // 定义position的位置为初始位置0
+            long position = 0;
+            // 获取文件总的字节数
+            long count = fromChannel.size();
+            // 从fromChannel通道缓冲区position位置开始读取count字节数写入到目标通道toChannel中
+            fromChannel.transferTo(position, count, toChannel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
