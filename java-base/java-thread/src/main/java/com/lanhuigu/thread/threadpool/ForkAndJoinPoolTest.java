de5c9e6e8ebf1a040f81d32b0f719c2b8c2936d1
yihonglei <yihonglei@baofeng.com>
2018-05-24 21:47:15 +0800

    fork-join框架

---
+++ java-base/java-thread/src/main/java/com/lanhuigu/thread/threadpool/ForkAndJoinPoolTest.java
@@ -0,0 +1,16 @@
+package com.lanhuigu.thread.threadpool;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * @author yihonglei
+ * @ClassName: ForkAndJoinPoolTest
+ * @Package: com.lanhuigu.thread.threadpool
+ * @date 2018/5/21 14:17
+ * @version 1.0.0
+ */
+public class ForkAndJoinPoolTest {
+    public static void main(String[] args) {
+        ForkJoinPool;
+    }
+}
