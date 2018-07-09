30c90d24651645f0811029d709beaf16e137a547
yihonglei <yihonglei@baofeng.com>
2018-07-09 08:24:04 +0800

    测试类

---
+++ java-base/java/src/main/java/com/lanhuigu/java/Test.java
@@ -0,0 +1,16 @@
+package com.lanhuigu.java;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+/**
+ * @author yihonglei
+ * @version 1.0.0
+ * @ClassName: Test
+ * @Package: com.lanhuigu.java
+ * @date 2018/7/8 17:16
+ */
+public class Test {
+    public static void main(String sgf[]) {
+    }
+
+}
