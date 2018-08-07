3353b9d01825c3003c309f61382580d8add194ba
yihonglei <yihonglei@baofeng.com>
2018-08-07 21:19:09 +0800

    test

--- java-base/java-thread/src/main/java/com/lanhuigu/thread/threadpool/Test.java
+++ java-base/java-thread/src/main/java/com/lanhuigu/thread/threadpool/Test.java
@@ -1,5 +1,9 @@
 package com.lanhuigu.thread.threadpool;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -14,5 +18,9 @@ public class Test {
     public static void main(String[] args) {
 //        ReentrantLock;
 //        ReentrantReadWriteLock;
+//        Executors;
+        Executor;
+        Future;
+        Callable;
     }
 }
