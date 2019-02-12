11fb4b4e017a93b0c91fe65099f6f68019d598f6
yihonglei <yihonglei@bcpeople.com>
2019-02-12 17:14:41 +0800

    等待,通知线程

--- java-base/java-thread/src/main/java/com/lanhuigu/thread/reentrantlock/demo2/MyService.java
+++ java-base/java-thread/src/main/java/com/lanhuigu/thread/reentrantlock/demo2/MyService.java
@@ -5,6 +5,7 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MyService {
+
     private Lock lock = new ReentrantLock();
     private Condition condition = lock.newCondition();
 
