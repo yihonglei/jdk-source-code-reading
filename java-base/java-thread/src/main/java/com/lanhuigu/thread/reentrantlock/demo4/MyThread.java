049b69a5d96be35d8edb59ec53738b80554fccbb
yihonglei <yihonglei@bcpeople.com>
2019-02-12 17:29:58 +0800

    ReentrantLock的Condition实现生产/消费模式实例增加

---
+++ java-base/java-thread/src/main/java/com/lanhuigu/thread/reentrantlock/demo4/MyThread.java
@@ -0,0 +1,20 @@
+package com.lanhuigu.thread.reentrantlock.demo4;
+
+/**
+ * @author yihonglei
+ * @date 2019/2/12 17:16
+ */
+public class MyThread extends Thread {
+    private Task task;
+
+    public MyThread(Task task) {
+        this.task = task;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("run方法begin");
+        task.await();
+        System.out.println("run方法end");
+    }
+}
