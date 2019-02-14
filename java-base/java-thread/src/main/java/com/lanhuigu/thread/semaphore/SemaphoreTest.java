f2641b10f48c0cec29567e7f9d15569e8a19e88f
yihonglei <yihonglei@bcpeople.com>
2019-02-14 19:08:46 +0800

    Semaphore(信号量)

--- java-base/java-thread/src/main/java/com/lanhuigu/thread/semaphore/SemaphoreTest.java
+++ java-base/java-thread/src/main/java/com/lanhuigu/thread/semaphore/SemaphoreTest.java
@@ -3,46 +3,21 @@ package com.lanhuigu.thread.semaphore;
 import java.util.concurrent.*;
 
 /**
- * Semaphore实例演示
+ * Semaphore（信号量）测试
+ *
  * @author yihonglei
  * @date 2018/8/5 11:35
  */
 public class SemaphoreTest {
-    private static final int THREAD_COUNT = 30;
-
-    private static Semaphore s = new Semaphore(10);
-
     public static void main(String[] args) {
-        /** 自定义线程池 */
-        int coreSize = THREAD_COUNT;
-        int maxThreads = THREAD_COUNT;
-        int keepAliveTime = 60;
-        TimeUnit timeUnit = TimeUnit.SECONDS;
-        LinkedBlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>(1024);
-        ThreadFactory threadFactory = new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable r) {
-                return new Thread();
+        try {
+            Toilet toilet = new Toilet();
+            ExecutorService executorService = Executors.newFixedThreadPool(10);
+            for (int i = 1; i <= 30; i ++) {
+                executorService.execute(new ShiftThread(toilet, i));
             }
-        };
-        ExecutorService executorService = new ThreadPoolExecutor(coreSize, maxThreads, keepAliveTime, timeUnit, blockingQueue, threadFactory);
-
-
-        for (int i =0; i <THREAD_COUNT; i++) {
-            // 提交任务
-            executorService.execute(()-> {
-                try {
-                    // 获取信号量
-                    s.acquire();
-                    System.out.println("逻辑处理--save data");
-                    // 释放信号量
-                    s.release();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        // 关闭线程池
-        executorService.shutdown();
     }
 }
---
+++ java-base/java-thread/src/main/java/com/lanhuigu/thread/semaphore/ShiftThread.java
@@ -0,0 +1,34 @@
+package com.lanhuigu.thread.semaphore;
+
+/**
+ * 大便！！！（画面感很强！）
+ *
+ * @author yihonglei
+ * @date 2019/2/14 17:19
+ */
+public class ShiftThread extends Thread {
+    private Toilet toilet;
+    private Integer num;
+
+    public ShiftThread(Toilet toilet, Integer num) {
+        this.toilet = toilet;
+        this.num = num;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // 获得坑
+            Toilet.Pit pitAcquire = toilet.getPit();
+            System.out.println("序号：" + num + "， " +pitAcquire.getDesc());
+            // 解决大号
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // 释放坑
+            Toilet.Pit pitRelealse = toilet.releasePit();
+            System.out.println("序号：" + num + "， " + pitRelealse.getDesc());
+        }
+    }
+}
