abc3e10b1b0e3c8090978888a6c27e98ab862077
yihonglei <yihonglei@bcpeople.com>
2019-04-13 23:29:32 +0800

    nio

--- java-base/java-nio/src/main/java/com/lanhuigu/nio/selectors/NIOClient.java
+++ java-base/java-nio/src/main/java/com/lanhuigu/nio/selectors/NIOClient.java
@@ -31,40 +31,49 @@ public class NIOClient {
     private void initClient(String ip, int port) throws IOException {
         // 获得一个Socket通道
         SocketChannel channel = SocketChannel.open();
+
         // 设置通道为非阻塞
         channel.configureBlocking(false);
+
         // 获得一个通道管理器
         this.selector = Selector.open();
+
         // 用channel.finishConnect();才能完成连接
         // 客户端连接服务器,其实方法执行并没有实现连接，需要在listen()方法中调
         channel.connect(new InetSocketAddress(ip, port));
+
         // 将通道管理器和该通道绑定，并为该通道注册SelectionKey.OP_CONNECT事件
         channel.register(selector, SelectionKey.OP_CONNECT);
     }
 
     /**
      * 采用轮询的方式监听selector上是否有需要处理的事件，如果有，则进行处理
-     *
-     * @throws Exception
      */
     @SuppressWarnings("unchecked")
     private void listen() throws Exception {
         // 轮询访问selector
         while (true) {
+            // 停顿下，为了观察效果
+            Thread.sleep(3000);
+
             /*
              * 选择一组可以进行I/O操作的事件，放在selector中,客户端的该方法不会阻塞，
              * selector的wakeup方法被调用，方法返回，而对于客户端来说，通道一直是被选中的
              * 这里和服务端的方法不一样，查看api注释可以知道，当至少一个通道被选中时。
              */
             selector.select();
+
             // 获得selector中选中的项的迭代器
             Set<SelectionKey> selectionKeys = this.selector.selectedKeys();
+
             // 获得selector中选中的相的迭代器，选中的相为注册的事件
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 // 删除已选的key，以防重复处理
                 iterator.remove();
+
                 // 连接事件发生
                 if (key.isConnectable()) {
                     // 如果正在连接，则完成连接
@@ -72,14 +81,17 @@ public class NIOClient {
                     if (channel.isConnectionPending()) {
                         channel.finishConnect();
                     }
+
                     // 设置成非阻塞
                     channel.configureBlocking(false);
+
                     // 在这里可以给服务端发送信息哦
-                    channel.write(ByteBuffer.wrap(new String("hello server!").getBytes()));
+                    channel.write(ByteBuffer.wrap(new String("hello server").getBytes()));
+
                     // 在和服务端连接成功之后，为了可以接收到服务端的信息，需要给通道设置读的权限。
-                    // 获得了可读的事件
                     channel.register(this.selector, SelectionKey.OP_READ);
-                } else if (key.isReadable()) {
+
+                } else if (key.isReadable()) {// 获得了可读的事件
                     read(key);
                 }
             }
@@ -93,9 +105,13 @@ public class NIOClient {
     private void read(SelectionKey key) throws Exception {
         SocketChannel channel = (SocketChannel) key.channel();
         // 分配缓冲区
-        ByteBuffer buffer = ByteBuffer.allocate(10);
+        ByteBuffer buffer = ByteBuffer.allocate(20);
         channel.read(buffer);
         byte[] data = buffer.array();
+
+        // 清空缓冲区
+        buffer.clear();
+
         String msg = new String(data).trim();
         System.out.println("client receive msg from server:" + msg);
         ByteBuffer outBuffer = ByteBuffer.wrap(msg.getBytes());
--- java-base/java-nio/src/main/java/com/lanhuigu/nio/selectors/NIOServer.java
+++ java-base/java-nio/src/main/java/com/lanhuigu/nio/selectors/NIOServer.java
@@ -29,12 +29,16 @@ public class NIOServer {
     private void initServer(int port) throws Exception {
         // 获取一个ServerSocket通道
         ServerSocketChannel serverChannel = ServerSocketChannel.open();
+
         // 设置通道为非阻塞
         serverChannel.configureBlocking(false);
+
         // 将该通道对于的serverSocket绑定到port端口
         serverChannel.socket().bind(new InetSocketAddress(port));
+
         // 获得一个通道管理器(选择器)
         this.selector = Selector.open();
+
         /**
          * 将通道管理器和该通道绑定，并为该通道注册selectionKey.OP_ACCEPT事件
          * 注册该事件后，当事件到达的时候，selectors.select()会返回，
@@ -44,35 +48,47 @@ public class NIOServer {
     }
 
     /**
-     * 采用轮询的方式监听selector上是否有需要处理的事件，如果有，进行处理
+     * 采用轮询的方式监听selector上是否有需要处理的事件，如果有，进行处理。
      */
     private void listen() throws Exception {
         System.out.println("start server");
         // 轮询访问selector
         while (true) {
+            // 停顿下，为了观察效果
+            Thread.sleep(3000);
+
             // 当注册事件到达时，方法返回，否则该方法会一直阻塞
             selector.select();
 
+            // 获取监听事件
             Set<SelectionKey> selectionKeys = this.selector.selectedKeys();
+
             // 获得selector中选中的相的迭代器，选中的相为注册的事件
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
+            // 迭代处理事件
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 // 删除已选的key以防重负处理
                 iterator.remove();
-                // 客户端请求连接事件
+
+                // 客户端请求连接事件，客户端连接成功
                 if (key.isAcceptable()) {
                     ServerSocketChannel server = (ServerSocketChannel) key.channel();
+
                     // 获得和客户端连接的通道
                     SocketChannel channel = server.accept();
+
                     // 设置成非阻塞
                     channel.configureBlocking(false);
+
                     // 在这里可以发送消息给客户端
                     channel.write(ByteBuffer.wrap(new String("hello client").getBytes()));
+
                     // 在客户端连接成功之后，为了可以接收到客户端的信息，需要给通道设置读的权限
                     channel.register(this.selector, SelectionKey.OP_READ);
-                    // 获得了可读的事件
-                } else if (key.isReadable()) {
+
+                } else if (key.isReadable()) { // 获得了可读的事件
                     read(key);
                 }
             }
@@ -85,10 +101,15 @@ public class NIOServer {
     private void read(SelectionKey key) throws Exception {
         // 服务器可读消息，得到事件发生的socket通道
         SocketChannel channel = (SocketChannel) key.channel();
+
         // 读取的缓冲区
-        ByteBuffer buffer = ByteBuffer.allocate(10);
+        ByteBuffer buffer = ByteBuffer.allocate(20);
         channel.read(buffer);
         byte[] data = buffer.array();
+
+        // 清空缓冲区
+        buffer.clear();
+
         String msg = new String(data).trim();
         System.out.println("server receive from client: " + msg);
         ByteBuffer outBuffer = ByteBuffer.wrap(msg.getBytes());
