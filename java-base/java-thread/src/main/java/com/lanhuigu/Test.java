fd184ddcbc1f33ec3b31ac31cdcf64cad21515d3
yihonglei <18701691306@163.com>
2019-07-30 20:41:55 +0800

    pom.xml

---
+++ java-base/java-thread/src/main/java/com/lanhuigu/Test.java
@@ -0,0 +1,12 @@
+package com.lanhuigu;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Test {
+    public static void main(String[] args) {
+        Collection;
+        ArrayList;
+    }
+}
---
+++ java-base/java/src/main/java/com/lanhuigu/java/map/ConcurrentHashMapTest.java
@@ -0,0 +1,11 @@
+package com.lanhuigu.java.map;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapTest {
+    public static void main(String[] args) {
+        ConcurrentHashMap ch = new ConcurrentHashMap();
+        ch.put("hh", "jj");
+        ch.get("hh");
+    }
+}
