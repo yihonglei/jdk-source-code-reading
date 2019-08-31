11082f60c7591a81c1652462f05634eb16ab602a
yihonglei <yihonglei@hqzhuanche.com>
2019-08-31 22:02:10 +0800

    java8

---
+++ java-base/java8/src/main/java/com/lanhuigu/java8/Test.java
@@ -0,0 +1,9 @@
+package com.lanhuigu.java8;
+
+/**
+ * @author: yihonglei
+ * @date: 2019-08-31 15:33
+ */
+public class Test {
+
+}
---
+++ java-base/java8/src/main/java/com/lanhuigu/java8/method/HiddleFile.java
@@ -0,0 +1,18 @@
+package com.lanhuigu.java8.method;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * 打印文件隐藏名
+ *
+ * @author: yihonglei
+ */
+public class HiddleFile {
+    public static void main(String[] args) {
+        // 当前目录下的隐藏文件
+        File[] files = new File(".").listFiles(File::isHidden);
+
+        Arrays.stream(files).forEach(x -> System.out.println(x.getName()));
+    }
+}
