32d8c649365b7bdcad071dc1af59da3f6ed374cf
yihonglei <yihonglei@hqzhuanche.com>
2019-09-17 20:24:54 +0800

    自定义类加载器

--- java-base/java-jvm/src/main/java/com/lanhuigu/jvm/loadclass/ClassLoaderTest.java
+++ java-base/java-jvm/src/main/java/com/lanhuigu/jvm/loadclass/ClassLoaderTest.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * 类加载器与instanceof关键字演示
+ * 类加载器与instanceof关键字演示：
+ *
+ * obj为自定义累加器加载对象，而ClassLoaderTest为另外一个类加载器加载的，所以，通过instanceof比较返回结果为false。
  */
 public class ClassLoaderTest {
     public static void main(String[] args) throws Exception {
