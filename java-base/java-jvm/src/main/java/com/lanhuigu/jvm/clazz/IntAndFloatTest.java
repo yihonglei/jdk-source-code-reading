9320fb73b2056c4078e5c26feec5e037eb91ad52
yihonglei <yihonglei@hqzhuanche.com>
2019-09-23 18:16:12 +0800

    jvm分析

---
+++ java-base/java-jvm/src/main/java/com/lanhuigu/jvm/clazz/IntAndFloatTest.java
@@ -0,0 +1,12 @@
+package com.lanhuigu.jvm.clazz;
+
+/**
+ * 字节码文件
+ */
+public class IntAndFloatTest {
+    private final int a = 10;
+    private final int b = 10;
+    private float c = 11f;
+    private float d = 11f;
+    private float e = 11f;
+}
--- java-base/java-jvm/src/main/java/com/lanhuigu/jvm/clazz/Test.java
+++
@@ -1,9 +0,0 @@
-package com.lanhuigu.jvm.clazz;
-
-/**
- * @author yihonglei
- * @date 2018/4/28 14:26
- */
-public class Test {
-    
-}
