9316b85142e394f9fc8f04351dff5e52549124c4
yihonglei <yihonglei@hqzhuanche.com>
2019-09-24 11:33:55 +0800

    类加载器

--- java-base/java-jvm/src/main/java/com/lanhuigu/jvm/loadclass/JDKClassLoader.java
+++ java-base/java-jvm/src/main/java/com/lanhuigu/jvm/loadclass/JDKClassLoader.java
@@ -1,6 +1,8 @@
 package com.lanhuigu.jvm.loadclass;
 
-
+/**
+ * jdk自带类加载器器
+ */
 public class JDKClassLoader {
     public static void main(String[] args) {
         System.out.println(String.class.getClassLoader());
