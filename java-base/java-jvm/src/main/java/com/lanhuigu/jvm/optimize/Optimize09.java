cae54bd8983282b8f905cf0c2969c7acccda0d73
yihonglei <yihonglei@hqzhuanche.com>
2019-09-24 13:55:05 +0800

    -

---
+++ java-base/java-jvm/src/main/java/com/lanhuigu/jvm/optimize/Optimize09.java
@@ -0,0 +1,22 @@
+package com.lanhuigu.jvm.optimize;
+
+/**
+ * 乘法/除法等采用移位操作
+ */
+public class Optimize09 {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i += 5) {
+            int a = i * 8;
+            int b = i / 2;
+        }
+        System.out.println("first耗时:" + (System.currentTimeMillis() - start));
+
+        long startS = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i += 5) {
+            int c = i << 3;
+            int d = i >> 1;
+        }
+        System.out.println("second耗时:" + (System.currentTimeMillis() - startS));
+    }
+}
