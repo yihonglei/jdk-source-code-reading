5bbb19caa93cbd452ec8b986d67555e3e0e40185
yihonglei <yihonglei@hqzhuanche.com>
2019-11-09 22:17:09 +0800

    项目结构重新调整

--- Java8InAction/src/main/java/lambdasinaction/appd/Lambda.java
+++
@@ -1,7 +0,0 @@
-package lambdasinaction.appd;
-
-import java.util.function.Function;
-
-public class Lambda {
-    Function<Object, String> f = obj -> obj.toString();
-}
---
+++ java-thread/src/main/java/com/lanhuigu/thread/utils/ThreadUtils.java
@@ -0,0 +1,10 @@
+package com.lanhuigu.thread.utils;
+
+/**
+ * 线程工具
+ *
+ * @author yihonglei
+ * @date 2019/11/8 10:53 AM
+ */
+public class ThreadUtils {
+}
