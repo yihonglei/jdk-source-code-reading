2e0802465e76d8976156f944a5c4c84bc0dbff06
yihonglei <yihonglei@hqzhuanche.com>
2020-10-03 15:35:30 +0800

    add annotation

---
+++ java-annotation/src/main/java/com/jpeony/annotation/AnnotationTest.java
@@ -0,0 +1,11 @@
+package com.jpeony.annotation;
+
+/**
+ * @author yihonglei
+ */
+public class AnnotationTest {
+    @MyAnnotation(value1 = "添加变量", value2 = MyEnum.Sunny, value3 = {"a", "b"})
+    public void execute() {
+        System.out.println("method");
+    }
+}
