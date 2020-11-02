5f01ade850bf5dbf5a67ff436523d13c49185bcb
yihonglei <yihonglei@hqzhuanche.com>
2020-11-02 20:38:30 +0800

    链表

--- java-collection/src/main/java/com/jpeony/collection/list/LinkedListTest.java
+++ java-collection/src/main/java/com/jpeony/collection/list/LinkedListTest.java
@@ -4,17 +4,21 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * LinkedList 链表实现
+ * LinkedList 基于双向链表实现
  *
  * @author yihonglei
  */
 public class LinkedListTest {
     public static void main(String[] args) {
-        List<String> list = new LinkedList<>();
-
+        LinkedList<String> list = new LinkedList<>();
+        // add
         list.add("one");
         list.add("two");
         list.add("three");
+        // offer的底层是add
+        list.offer("four");
+        // pop出队
+        // list.pop();
 
         for (int i = 0; i < list.size(); i++) {
             System.out.println("element[" + i + "] " + list.get(i));
