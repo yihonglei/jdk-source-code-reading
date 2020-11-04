3b5b3e8ff12b872b6fadbcd908efc253760d1915
yihonglei <yihonglei@hqzhuanche.com>
2020-11-04 19:41:27 +0800

    stack

--- java-collection/src/main/java/com/jpeony/collection/stack/StackTest.java
+++ java-collection/src/main/java/com/jpeony/collection/stack/StackTest.java
@@ -1,10 +1,19 @@
 package com.jpeony.collection.stack;
 
+import java.util.Stack;
+
 /**
  * @author yihonglei
  */
 public class StackTest {
     public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
 
+        // 入栈
+        stack.push("one");
+        stack.push("two");
+
+        // 出栈
+        System.out.println(stack.pop());
     }
 }
