a229983ca8d8d20e9e7b930e1bb10da67bc08407
yihonglei <yihonglei@hqzhuanche.com>
2020-12-21 20:31:17 +0800

    common and ArrayBlockingQueue

---
+++ java-concurrent/src/main/java/com/jpeony/concurrent/queue/ArrayBlockingQueueTest.java
@@ -0,0 +1,15 @@
+package com.jpeony.concurrent.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * @author yihonglei
+ */
+public class ArrayBlockingQueueTest {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayBlockingQueue<String> arrayQueue = new ArrayBlockingQueue<>(2);
+        arrayQueue.offer("one");
+
+        System.out.println(arrayQueue.take());
+    }
+}
---
+++ java-concurrent/src/main/java/com/jpeony/concurrent/queue/SynchronousQueueTest.java
@@ -0,0 +1,8 @@
+package com.jpeony.concurrent.queue;
+
+/**
+ * @author yihonglei
+ */
+public class SynchronousQueueTest {
+    
+}
