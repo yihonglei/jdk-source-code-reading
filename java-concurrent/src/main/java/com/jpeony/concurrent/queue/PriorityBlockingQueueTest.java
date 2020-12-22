993569b622d3e05f2d5f6fad61e28519659a59ca
yihonglei <yihonglei@hqzhuanche.com>
2020-12-22 20:54:09 +0800

    PriorityBlockingQueue

--- java-concurrent/src/main/java/com/jpeony/concurrent/queue/PriorityBlockingQueueTest.java
+++ java-concurrent/src/main/java/com/jpeony/concurrent/queue/PriorityBlockingQueueTest.java
@@ -1,8 +1,18 @@
 package com.jpeony.concurrent.queue;
 
+import java.util.concurrent.PriorityBlockingQueue;
+
 /**
  * @author yihonglei
  */
 public class PriorityBlockingQueueTest {
+    public static void main(String[] args) throws InterruptedException {
+        PriorityBlockingQueue<String> priorityQueue = new PriorityBlockingQueue<>(2);
 
+        priorityQueue.offer("one");
+        priorityQueue.offer("two");
+        priorityQueue.offer("three");
+
+        System.out.println(priorityQueue.take());
+    }
 }
