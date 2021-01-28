112e1fe415679722e53f7d400bc46a689b1fa274
yihonglei <yihonglei@hqzhuanche.com>
2021-01-28 20:38:30 +0800

    ComparableTimeSort

--- java-collection/src/main/java/com/jpeony/collection/list/ArrayListTest.java
+++ java-collection/src/main/java/com/jpeony/collection/list/ArrayListTest.java
@@ -20,7 +20,10 @@ public class ArrayListTest {
         }
 
         System.out.println("=== sort ===");
-        list.sort(new ListComparator());
+        // ComparableTimSort.sort 逻辑
+        list.sort(null);
+        // TimSort.sort 逻辑
+//        list.sort(new ListComparator());
 
         for (int i = 0; i < list.size(); i++) {
             System.out.println("element[" + i + "] = " + list.get(i));
