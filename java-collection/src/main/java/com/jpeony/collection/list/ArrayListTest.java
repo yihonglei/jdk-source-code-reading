8178ede488d577325483e40fc6fc3235e5982d52
yihonglei <yihonglei@hqzhuanche.com>
2021-01-29 11:51:36 +0800

    ComparableTimeSort

--- java-collection/src/main/java/com/jpeony/collection/list/ArrayListTest.java
+++ java-collection/src/main/java/com/jpeony/collection/list/ArrayListTest.java
@@ -12,8 +12,11 @@ public class ArrayListTest {
         List<Integer> list = new ArrayList<>();
 
         list.add(1);
-        list.add(3);
         list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(6);
+        list.add(5);
 
         for (int i = 0; i < list.size(); i++) {
             System.out.println("element[" + i + "] = " + list.get(i));
