0517c68011f13aebeaabf700c3458b6f06668386
yihonglei <yihonglei@hqzhuanche.com>
2021-02-01 20:59:33 +0800

    LinkedTransferQueue

--- java-concurrent/src/main/java/com/jpeony/concurrent/queue/LinkedTransferQueueTest.java
+++ java-concurrent/src/main/java/com/jpeony/concurrent/queue/LinkedTransferQueueTest.java
@@ -1,8 +1,15 @@
 package com.jpeony.concurrent.queue;
 
+import java.util.concurrent.LinkedTransferQueue;
+
 /**
  * @author yihonglei
  */
 public class LinkedTransferQueueTest {
+    public static void main(String[] args) {
+        LinkedTransferQueue<String> queue = new LinkedTransferQueue<>();
 
+        queue.add("one");
+        queue.add("two");
+    }
 }
