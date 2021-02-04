2eff4e7b9d41192947f057064bebffafda08b280
yihonglei <yihonglei@hqzhuanche.com>
2021-02-04 16:46:20 +0800

    thread local

--- java-concurrent/src/main/java/com/jpeony/concurrent/threadlocal/RunSimple.java
+++ java-concurrent/src/main/java/com/jpeony/concurrent/threadlocal/RunSimple.java
@@ -7,13 +7,14 @@ package com.jpeony.concurrent.threadlocal;
  */
 public class RunSimple {
     private static ThreadLocal THREAD_LOCAL = new ThreadLocal();
+    private static ThreadLocal THREAD_LOCAL2 = new ThreadLocal();
 
     public static void main(String[] args) {
-        if (THREAD_LOCAL.get() == null) {
-            System.out.println("未放入值");
-            THREAD_LOCAL.set("放入值");
-        }
+        // 放入值
+        THREAD_LOCAL.set("放入值");
+        THREAD_LOCAL2.set("放入值2");
         // 获取值
-        System.out.println("print：" + THREAD_LOCAL.get());
+        System.out.println("print-thread-local-1：" + THREAD_LOCAL.get());
+        System.out.println("print-thread-local-2：" + THREAD_LOCAL2.get());
     }
 }
