0c3787b6010ec4b8abc8425cc2935e3ece38b5f3
yihonglei <yihonglei@hqzhuanche.com>
2021-02-24 19:43:45 +0800

    queue

--- java-concurrent/src/main/java/com/jpeony/concurrent/queue/LinkedBlockingQueueTest.java
+++ java-concurrent/src/main/java/com/jpeony/concurrent/queue/LinkedBlockingQueueTest.java
@@ -20,14 +20,17 @@ public class LinkedBlockingQueueTest {
         // 存入元素
         // offer() 非阻塞，队列满了就放不进去，也不会等着空的时候再放，直接返回 false；
         blockingQueue.offer("object01");
+//        blockingQueue.offer("object02");
+//        blockingQueue.offer("object03");
 
         // put() 阻塞，当队列满了，一直等着啥时候空了，啥时候放，很执着；
-        blockingQueue.put("object02");
+//        blockingQueue.put("object02");
 
         // take() 阻塞，当队列没有元素的时候，阻塞当前线程，直到队列不为空；
         logger.info("blockingQueue.take()={}", blockingQueue.take());
+//        blockingQueue.take();
 
         // poll 非阻塞，队列为空时，直接退出，不进行阻塞操作；
-        logger.info("blockingQueue.poll()={}", blockingQueue.poll());
+//        logger.info("blockingQueue.poll()={}", blockingQueue.poll());
     }
 }
