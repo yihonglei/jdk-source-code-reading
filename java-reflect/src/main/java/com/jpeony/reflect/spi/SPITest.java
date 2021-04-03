04eb42b33e8780a97b52d8924e073539f908a058
yihonglei <yihonglei@hqzhuanche.com>
2021-04-03 22:27:42 +0800

    service

--- java-reflect/src/main/java/com/jpeony/reflect/spi/SPITest.java
+++ java-reflect/src/main/java/com/jpeony/reflect/spi/SPITest.java
@@ -10,15 +10,15 @@ import java.util.ServiceLoader;
  */
 public class SPITest {
     public static void main(String[] args) {
-        Iterator<SPIService> providers = Service.providers(SPIService.class);
-        ServiceLoader<SPIService> load = ServiceLoader.load(SPIService.class);
         // 方式1
+        Iterator<SPIService> providers = Service.providers(SPIService.class);
         while (providers.hasNext()) {
             SPIService ser = providers.next();
             ser.execute();
         }
         System.out.println("--------------------------------");
         // 方式2
+        ServiceLoader<SPIService> load = ServiceLoader.load(SPIService.class);
         Iterator<SPIService> iterator = load.iterator();
         while (iterator.hasNext()) {
             SPIService ser = iterator.next();
