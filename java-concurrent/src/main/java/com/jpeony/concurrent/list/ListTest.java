8b16d0c06d6e68ae545fd1ab242c7f26c2a80fda
yihonglei <yihonglei@hqzhuanche.com>
2021-04-29 16:51:07 +0800

    list

--- java-concurrent/src/main/java/com/jpeony/concurrent/list/ListTest.java
+++ java-concurrent/src/main/java/com/jpeony/concurrent/list/ListTest.java
@@ -10,6 +10,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * 1、List为继承于Collection的一个子接口（可查看源码）
  * 2、List元素有放入顺序，可重复
  * 3、常用实现类（ArrayList，LinkedList，Vector，CopyOnWriteArrayList）
+ * <p>
  * ArrayList 非线程安全，效率高；
  * LinkedList 底层基于链表实现，结构散乱；
  * Vector 线程安全，效率低；
@@ -19,39 +20,67 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class ListTest {
     public static void main(String[] args) {
-        // 1、ArrayList 特征：非线程安全，元素有顺序，高效，可重复
+        /*
+         * ArrayList
+         * 【数据结构】
+         *  底层基于数组实现。
+         *
+         * 【特性】
+         *  支持根据下标随机访问时间复杂度 O(1)，插入和删除时间复杂度 O(n)，因为要进行数据元素搬移；
+         *  非线程安全，元素有序，可以重复；
+         */
         ArrayList<String> list = new ArrayList();
         list.add("a");
         list.add("b");
         list.add("c");
         list.add("d");
-        // 可以重复检测
+
         list.add("d");
-        // 循环输出list中的元素，下表从0开始
         for (String s : list) {
             System.out.println("ArrayList:" + s);
         }
-        // 2、LinkedList底层基于链表实现，结构散乱，每一个元素存储本身地址的同时还存下一个元素的地址。 特征：链表增删快，查找慢。
+        /*
+         * LinkedList
+         * 【数据结构】
+         *  底层基于双向链表实现。
+         *
+         * 【特性】
+         * 插入和删除时间复杂度 O(1)，不支持随机查找元素，元素查找需要遍历链表，时间复杂度 O(n)
+         */
         LinkedList<String> list2 = new LinkedList();
         list2.add("a2");
         list2.add("b2");
         list2.add("c2");
         list2.add("d2");
-        // 循环输出list2中的元素，下标从0开始
+
         for (String s : list2) {
             System.out.println("LinkedList:" + s);
         }
-        // 3、Vector 特征：线程安全，低效。
+        /*
+         * Vector
+         * 【数据结构】
+         * 底层基于数组实现。
+         *
+         * 【特性】
+         *  synchronized 加锁保证线程安全，低效。
+         */
         Vector<String> list3 = new Vector();
         list3.add("a3");
         list3.add("b3");
         list3.add("c3");
         list3.add("d3");
-        // 循环输出list3中的元素，下标从0开始
+
         for (String s : list3) {
             System.out.println("Vector:" + s);
         }
-        // 4、CopyOnWriteArrayList 特征：线程安全、读写分离场景，高效。
+        /*
+         * CopyOnWriteArrayList
+         * 【数据结构】
+         * 底层基于数组实现。
+         *
+         * 【特性】
+         *  线程安全、读写分离场景，用于读多写少场景；
+         */
         List<String> list4 = new CopyOnWriteArrayList<>();
         list4.add("a1");
         list4.add("a2");
