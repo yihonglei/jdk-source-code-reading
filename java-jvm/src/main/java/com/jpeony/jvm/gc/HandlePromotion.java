71cb2eca3eca011a2fcc1d87c108f5f7a0482d8c
yihonglei <jpeony@163.com>
2021-12-06 18:53:14 +0800

    common

--- java-jvm/src/main/java/com/jpeony/jvm/gc/HandlePromotion.java
+++ java-jvm/src/main/java/com/jpeony/jvm/gc/HandlePromotion.java
@@ -1,25 +1,18 @@
 package com.jpeony.jvm.gc;
 
 /**
- * VM参数:
+ * JVM 参数配置:
  * -verbose:gc -XX:+PrintGCDetails -Xms20M -Xmx20M -Xmn10M -XX:SurvivorRatio=8 -XX:+HandlePromotionFailure
  *
  * 程序说明：
- * 一开始allocation1、allocation2、allocation3分配在eden区，同时allocation1被置为无效。
- *
- * 当第一次给allocation4分配内存时，eden区内存不够，发生一次Minor GC，
- *
- * 由于老年代的连续可用空间大于存活的对象总和，所以allocation2、allocation3将会进入老年代，
- *
- * allocation1的空间将被回收，此时整个eden区被清空，又变为8MB，有空间让allocation4分配在eden区；
- *
- * 接下来allocation5、allocation6接着往eden区分配，同时allocation4、allocation5、allocation6被置为无效。
- *
- * 当第二次给allocation7分配内存时，eden区内存不够，发生一次Minor GC，allocation4、allocation5、
- *
- * allocation6所占的内存全部回收，把整个eden区清空，变为8MB，然后将allocation7分配在新生代eden区。
- *
- * 最后，allocation2、allocation3在老年代，allocation7在新生代。
+ * 一开始 allocation1、allocation2、allocation3 分配在 eden 区，同时 allocation1 被置为无效。
+ * 当第一次给 allocation4 分配内存时，eden 区内存不够，发生一次 Minor GC，
+ * 由于老年代的连续可用空间大于存活的对象总和，所以 allocation2、allocation3 将会进入老年代，
+ * allocation1 的空间将被回收，此时整个 eden 区被清空，又变为 8MB，有空间让 allocation4 分配在 eden 区；
+ * 接下来 allocation5、allocation6 接着往 eden 区分配，同时 allocation4、allocation5、allocation6 被置为无效。
+ * 当第二次给 allocation7 分配内存时，eden 区内存不够，发生一次 Minor GC，allocation4、allocation5、
+ * allocation6 所占的内存全部回收，把整个 eden 区清空，变为 8MB，然后将 allocation7 分配在新生代 eden 区。
+ * 最后，allocation2、allocation3 在老年代，allocation7 在新生代。
  *
  * @author yihonglei
  */
