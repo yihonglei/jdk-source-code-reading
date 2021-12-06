666bd9668aa7c740262974eecded9a77f6718d9b
yihonglei <jpeony@163.com>
2021-12-06 15:13:40 +0800

    common

--- java-jvm/src/main/java/com/jpeony/jvm/gc/AllocationTest01.java
+++ java-jvm/src/main/java/com/jpeony/jvm/gc/AllocationTest01.java
@@ -1,18 +1,13 @@
 package com.jpeony.jvm.gc;
 
 /**
- * 优先分配在eden演示！
- *
- * VM参数: -verbose:gc -XX:+PrintGCDetails -Xmx20M -Xms20M -Xmn10M -XX:SurvivorRatio=8
- *
- * 参数说明:
- * -verbose:gc 输出显示虚拟机运行信息；
- *
- * -XX:+PrintGCDetails 打印内存回收日志；
- *
- * -Xmx20M -Xms20M -Xmn10M 限制堆大小为20M，不可以扩展，10M分配给新生代，剩下10分配给老年代；
- *
- * -XX:SurvivorRatio=8 配置Eden区与一个Survivor区的比例，这里是默认的8:1，不用显示配置也可以；
+ * 【优先分配在 eden 区】
+ * JVM 参数配置: -verbose:gc -XX:+PrintGCDetails -Xmx20M -Xms20M -Xmn10M -XX:SurvivorRatio=8
+ * JVM 参数说明:
+ * -verbose:gc 输出显示虚拟机运行信息；
+ * -XX:+PrintGCDetails 打印内存回收日志；
+ * -Xmx20M -Xms20M -Xmn10M 限制堆大小为 20M，不可以扩展，10M 分配给新生代，剩下 10M 分配给老年代；
+ * -XX:SurvivorRatio=8 配置 Eden 区与一个 Survivor 区的比例，这里是默认的 8:1，不用显示配置也可以；
  *
  * @author yihonglei
  */
