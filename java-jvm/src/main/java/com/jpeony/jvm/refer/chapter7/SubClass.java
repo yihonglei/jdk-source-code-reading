b4aec286392481091785e4e21b3bf9a91d329f10
yihonglei <jpeony@163.com>
2023-06-03 22:48:40 +0800

    common

---
+++ java-jvm/src/main/java/com/jpeony/jvm/refer/chapter7/SubClass.java
@@ -0,0 +1,11 @@
+package com.jpeony.jvm.refer.chapter7;
+
+/**
+ * @author zzm
+ */
+public class SubClass extends SuperClass {
+
+    static {
+        System.out.println("SubClass init!");
+    }
+}
