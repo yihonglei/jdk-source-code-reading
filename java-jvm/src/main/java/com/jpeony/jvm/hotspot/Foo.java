5be9906153a5a9b03bd3bf820804acd676d4a57f
yihonglei <jpeony@163.com>
2023-11-22 18:16:51 +0800

    jvm

---
+++ java-jvm/src/main/java/com/jpeony/jvm/hotspot/Foo.java
@@ -0,0 +1,9 @@
+package com.jpeony.jvm.hotspot;
+
+public class Foo {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = a + 2;
+        System.out.println(b);
+    }
+}
