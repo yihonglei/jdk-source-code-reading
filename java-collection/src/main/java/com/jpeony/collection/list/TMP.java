bc5259732cd19fc4341296f940b9f598d5756dc7
yihonglei <jpeony@163.com>
2024-01-13 21:13:36 +0800

    jvm

---
+++ java-collection/src/main/java/com/jpeony/collection/list/TMP.java
@@ -0,0 +1,13 @@
+package com.jpeony.collection.list;
+
+public class TMP {
+    public static void main(String[] args) {
+        System.out.println(1%3);
+        System.out.println(2%3);
+        System.out.println(3%3);// 0
+        System.out.println(4%3);
+        System.out.println(5%3);
+        System.out.println(6%3);// 0
+        System.out.println(7%3);
+    }
+}
