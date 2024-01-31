ecca4c0f28389b0c9e165d662e4d3cfe604a5233
yihonglei <jpeony@163.com>
2024-01-31 17:03:52 +0800

    ScheduledThreadPoolExecutor

--- java-concurrent/src/main/java/com/jpeony/concurrent/threadpool/NewScheduledThreadPoolTest.java
+++ java-concurrent/src/main/java/com/jpeony/concurrent/threadpool/NewScheduledThreadPoolTest.java
@@ -3,6 +3,7 @@ package com.jpeony.concurrent.threadpool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Date;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -19,7 +20,7 @@ public class NewScheduledThreadPoolTest {
     public static void main(String[] args) {
         // 线程池
         ScheduledThreadPoolExecutor ste = new ScheduledThreadPoolExecutor(1);
-        Runnable runnable = () -> logger.info("ThreadName={},Time={}", Thread.currentThread().getName());
+        Runnable runnable = () -> logger.info("ThreadName={},Time={}", Thread.currentThread().getName(), new Date());
         // 定时重复执行
         ste.scheduleAtFixedRate(runnable, 5, 5, TimeUnit.SECONDS);
         // 延时执行
